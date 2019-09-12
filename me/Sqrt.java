1a2f58d7c3aae4967cfaadfb4458b44863a5be46
lizzie4all <lizzie4all@yahoo.co.uk>
2019-09-12 18:20:18 -0500

    It Project

--- me/Sqrt.java
+++
@@ -1,7 +0,0 @@
-public class Sqrt {
-    public static void main(String[] args) {
-    	float a = (Math.Sqrt(-4));
-    	System.out.println(a);
-     
-    }
-}
--- me/import java.util.Scanner;.java
+++
@@ -1,7 +0,0 @@
-public class Sqrt {
-    public static void main(String[] args) {
-    	float Sq = Math.Sqrt(-4);
-    	System.out.println(Sq);
-     
-    }
-}
